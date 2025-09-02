377391c9ceb203b196dae332d70d5d6df4cd02d9
kosti846 <reguetton456@gmail.com>
2025-09-02 13:06:10 -0600

    feat(homepage): inicializa estructura SASS y primera sección

---
+++ src/components/pages/pages1.tsx
@@ -0,0 +1,13 @@
+import { Button } from '../atoms/inputs/Button/Button';
+
+export default function Home() {
+  return (
+    <section className="gym">
+      <h1 className="tittle1">NOSOTROS SOMOS GYMRAT</h1>
+
+      <h6 className="tittle2">tu mejor opcion en el mundo del fitness</h6>
+
+      <Button variant="contained">SÉ PARTE</Button>
+    </section>
+  );
+}
